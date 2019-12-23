de914a27c853ee7dfc3efae85c2667dbf7ded158
teddy <49538481+shnhrtkyk@users.noreply.github.com>
2019-12-23 00:09:56 +0000

    Add files via upload

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,4 +1,4 @@
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda-9.0/include -lcudart -L /usr/local/cuda-9.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -I $TF_INC/external/nsync/public -L $TF_LIB -ltensorflow_framework
+TF_INC=$(python3.6 -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python3.6 -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda/include -lcudart -L /usr/local/cuda/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0 -I $TF_INC/external/nsync/public -L $TF_LIB -ltensorflow_framework
 
